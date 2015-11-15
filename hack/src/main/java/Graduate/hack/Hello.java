bc24b626bfbf07485f916f2901af26a532d56c24
ClaudeWilliam <qjqandjack@gmail.com>
2015-11-15 10:57:12 +0800

    hello

---
+++ hack/src/main/java/Graduate/hack/Hello.java
@@ -0,0 +1,26 @@
+package Graduate.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+	@Controller
+	@EnableAutoConfiguration
+	public class Hello {
+
+	    @RequestMapping("/")
+	    @ResponseBody
+	    String home() {
+	        return "Hello World!";
+	    }
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(Hello.class, args);
+	    }
+	   
+	}
+	 
+
+
